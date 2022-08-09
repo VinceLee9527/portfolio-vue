00064c89a8a5164bb55f6cdd37198dd451abd4be
Vince <vlee9527@gmail.com>
2022-08-10 01:28:49 +0800

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col justify-around w-full items-center mt-5">
+    <div class="name-container">
+      <h1 class="name">Vincent Lee / 李崴霖</h1>
+    </div>
+    <div class="w-full">
+      <ul class="flex justify-around text-lg">
+        <li>
+          <router-link to="/"><span>about</span> </router-link>
+        </li>
+        <li>
+          <router-link to="/"><span>blog</span> </router-link>
+        </li>
+        <li>
+          <router-link to="/"><span>projects</span> </router-link>
+        </li>
+        <li>
+          <router-link to="/"><span>contact</span> </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  setup() {},
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+</style>
