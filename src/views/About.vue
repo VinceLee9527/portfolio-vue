2d33314cb2de76df200bd8165fcc1cd6171b4719
Vince <vlee9527@gmail.com>
2022-08-31 03:10:18 +0800

    about update

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,18 +4,30 @@
 <template>
   <p>
     My first encounter with a programming language began with an Amazon gift
-    card. My first job out of college was hosting a SQL bootcamp, where upon
-    completion you'd receive a houndred dollars Amazon gift card. This bootcamp
-    was intended for engineers, however the gift card and my passion for
-    learning new things made it a no brainer that I'd enroll in this bootcamp.
-    Since it was intended for people with coding foundation, the course was very
-    advanced. This means a person with 0 coding experience like myself would
-    have to spend the entire weekend in a coffee shop working on homework and
-    catching up. I'm not gonna lie, the 8 weeks of bootcamp seemed like hell,
-    and I was ready to quit every second. But I didn't. It might be the sense of
-    achievement when I finally successfully ran my query search, or I was
-    finding a lot of joy during the process, either way, I completed the
-    bootcamp.
+    card. My first job out of college was hosting a SQL boot camp, where upon
+    completion you'd receive a hundred dollars Amazon gift card. This boot camp
+    was intended for engineers, however, the gift card and my passion for
+    learning new things made it a no-brainer that I'd enroll in this boot camp.
+    Since it was intended for people with a coding foundation, the course was
+    very advanced. This means a person with 0 coding experience like myself
+    would have to spend the entire weekend in a coffee shop working on homework
+    and catching up. I'm not gonna lie, the 8 weeks of boot camp. seemed like
+    hell, and I was ready to quit every second. But I didn't. It might be the
+    sense of achievement when I finally successfully ran my query search, or I
+    was finding a lot of joy during the process, either way, I completed the
+    boot camp. I fell in love with the ability to problem solve and create with
+    programming languages, and remember thinking to myself how awesome it'd be
+    if I can do this for a living. Fast forward a year later, I decided to
+    pursue this dream and the rest is history. So let's fast-forward to the part
+    that you're interested in: What can I offer? With my extensive experience
+    working in tech startups, I excel in a fast-paced, high-pressure
+    environment. I'm quick to adapt to any changes and can juggle multiple tasks
+    and roles well. My biggest strength is responsibility. I take ownership of
+    what I do and constantly contribute with new ideas and ways to optimize. I
+    believe it is because of this attitude that I was promoted to run a 7 people
+    team within 8 months of my first job out of college. I understand that not
+    coming from a CS background means that I have to work twice as hard to catch
+    up. I'm constantly learning about new tech and am a very motivated learner.
   </p>
 </template>
 
