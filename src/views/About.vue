776671786c02d3535680ad3cbe6387ccb69bb2ba
Vince <vlee9527@gmail.com>
2022-10-03 23:11:56 +0800

    about revision

--- src/views/About.vue
+++ src/views/About.vue
@@ -62,6 +62,7 @@
       <div>
         <a
           href="https://drive.google.com/drive/folders/1oq8GaHgzyX151m5ExcPufZdKrT7NIKV2?usp=sharing"
+          target="blank"
           class="resume click"
           >Click Here</a
         >
@@ -71,22 +72,23 @@
       <h4 class="para-header">A little about myself</h4>
       <p class="para first">
         My first encounter with a programming language began with an Amazon gift
-        card. My first job out of college was hosting a SQL boot camp, where
-        upon completion you'd receive a hundred dollars Amazon gift card. This
-        boot camp was intended for engineers, however, the gift card and my
-        passion for learning new things made it a no-brainer that I'd enroll in
-        this boot camp. <br /><br />Since it was intended for people with a
-        coding foundation, the course was very advanced. This means a person
-        with 0 coding experience like myself would have to spend the entire
-        weekend in a coffee shop working on homework and catching up. I'm not
-        gonna lie, the 8 weeks of boot camp seemed like hell, and I was ready to
-        quit every second. But I didn't. It might be the sense of achievement
-        when I finally successfully ran my query search, or I was finding a lot
-        of joy during the process, either way, I completed the boot camp.
-        <br /><br />I fell in love with the ability to problem solve and create
-        with programming languages, and remember thinking to myself how awesome
-        it'd be if I can do this for a living. Fast forward a year later, I
-        decided to pursue this dream and the rest is history.
+        card. The company that I worked at at the time was hosting a SQL boot
+        camp, where upon completion you'd receive a hundred dollars Amazon gift
+        card. This boot camp was intended for engineers, however, the gift card
+        and my passion for learning new things made it a no-brainer that I'd
+        take on this challenge. <br /><br />Since it was intended for people
+        with a coding foundation, the course was very advanced. This means a
+        person with 0 coding experience like myself would have to spend the
+        entire weekend every week in a coffee shop working on homework and
+        catching up. I'm not gonna lie, the 8 weeks of boot camp seemed like
+        hell, and I was ready to quit every second. But I didn't. It might be
+        the sense of achievement when I finally successfully ran my query
+        search, or I was finding a lot of joy during the process, either way, I
+        completed the boot camp. <br /><br />I fell in love with the ability to
+        problem solve and create with programming languages, and remember
+        thinking to myself how awesome it'd be if I can do this for a living.
+        Fast forward a year later, I decided to pursue this dream and the rest
+        is history.
       </p>
       <h4 class="para-header">Why should you hire me?</h4>
       <p class="para second">
@@ -94,13 +96,16 @@
         offer? <br /><br />With my extensive experience working in tech
         startups, I excel in a fast-paced, high-pressure environment. I'm quick
         to adapt to any changes and can juggle multiple tasks and roles well. My
-        biggest strength is responsibility. I take ownership of what I do and
-        constantly contribute with new ideas and ways to optimize. I believe it
-        is because of this attitude that I was promoted to run a 7 people team
+        biggest strength is my sense of responsibility. I take ownership of what
+        I do and constantly contribute with new ideas and ways to optimize. I
+        believe that this attitude got me a promotion to run a 7 people team
         within 8 months of my first job out of college. <br /><br />I understand
         that not coming from a CS background means that I have to work twice as
-        hard to catch up. I'm constantly learning about new tech and am a very
-        motivated learner.
+        hard to catch up. I'm a very motivated learner and am constantly looking
+        out for new tech and tools. I'm also actively filling up on my
+        JavaScript knowledge to understand how this language works on the
+        backend. I'd love to have a chance to prove what I know I can do as a
+        Frontend Engineer.
       </p>
     </div>
   </div>
